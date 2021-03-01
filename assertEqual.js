0b19e671eb7e53cbce2fff448ea4d758c54278fa
Paul Ladd <vlendle@gmail.com>
2021-03-01 20:06:33 +0000

    Added quotation marks around string inputs.

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,14 @@
 const assertEqual = function(actual, expected) {
+  const actualStr = typeof(actual) === "string" ? `"${actual}"` : actual;
+  const expectedStr = typeof(expected) === "string" ? `"${expected}"` : expected;
   if (actual === expected) {
-    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion passed: ${actualStr} === ${expectedStr}`);
   } else {
-    console.log(`🔴🔴🔴Assertion failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴Assertion failed: ${actualStr} !== ${expectedStr}`);
   }
 };
 
 assertEqual("LighthouseLabs", "Bootcamp");
 assertEqual("pladd", "goober");
 assertEqual(1, 1);
+assertEqual("1", "1");
