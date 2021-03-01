79e73a4d66f23d90073b2bb4db04824e811e0398
Paul Ladd <vlendle@gmail.com>
2021-03-01 19:56:59 +0000

    Added emoji markers to the output.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
-  if(actual === expected) {
-    console.log(`Assertion passed: ${actual} === ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
