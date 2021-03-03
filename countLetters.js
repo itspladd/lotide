0df65e09deb0281985ebbf91c34d9e6ea9700ff4
Paul Ladd <vlendle@gmail.com>
2021-03-03 19:41:09 +0000

    Added shell function and assertEqual

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  const actualStr = typeof(actual) === "string" ? `"${actual}"` : actual;
+  const expectedStr = typeof(expected) === "string" ? `"${expected}"` : expected;
+  
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion passed: ${actualStr} === ${expectedStr}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion failed: ${actualStr} !== ${expectedStr}`);
+  }
+};
+
+const countLetters = function() {
+
+};
