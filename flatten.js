464c84a0e022c056395b9bd5734b83c580c21be2
Paul Ladd <vlendle@gmail.com>
2021-03-03 00:20:58 +0000

    Adding new flatten.js

---
+++ flatten.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  // If the two arrays aren't of the same length, return false before we iterate.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    // If at any point we find an unequal value, return false.
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // If we make it this far, then all of the values are equal and we can return true.
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅✅Assertion passed: ", array1, " === ", array2);
+  } else {
+    console.log("🔴🔴🔴Assertion failed: ", array1, " !== ", array2);
+  }
+}
+
+const flatten = function (arrayToFlatten) {
+  let flattenedArray = [];
+  for (let i = 0; i < arrayToFlatten.length; i++) {
+    if (Array.isArray(arrayToFlatten[i])) {
+      for (let j = 0; j < arrayToFlatten[i].length; j++) {
+        flattenedArray.push(arrayToFlatten[i][j]);
+      }
+    } else {
+      flattenedArray.push(arrayToFlatten[i]);
+    }
+  }
+  
+  return flattenedArray;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
