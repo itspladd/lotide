20d85f44843938bbefb4d353c1b58943fca378d7
Paul Ladd <vlendle@gmail.com>
2021-03-03 20:01:24 +0000

    Replaced assertEqual with assertArraysEqual and eqArrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,11 +1,24 @@
-const assertEqual = function(actual, expected) {
-  const actualStr = typeof(actual) === "string" ? `"${actual}"` : actual;
-  const expectedStr = typeof(expected) === "string" ? `"${expected}"` : expected;
-  
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion passed: ${actualStr} === ${expectedStr}`);
+const eqArrays = function(array1, array2) {
+  // If the two arrays aren't of the same length, return false before we iterate.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    // If at any point we find an unequal value, return false.
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // If we make it this far, then all of the values are equal and we can return true.
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅✅Assertion passed: ", array1, " === ", array2);
   } else {
-    console.log(`🔴🔴🔴Assertion failed: ${actualStr} !== ${expectedStr}`);
+    console.log("🔴🔴🔴Assertion failed: ", array1, " !== ", array2);
   }
 };
 
