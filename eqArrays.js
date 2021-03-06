9a684cfe0935794e38481612009c7623910e78cc
Paul Ladd <vlendle@gmail.com>
2021-03-06 02:39:24 +0000

    Added recursion handling and added the eqObjects function to handle objects in arrays (with bonus recursive object handling)

--- eqArrays.js
+++ eqArrays.js
@@ -9,6 +9,37 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  //If they're not the same size, then they're defintely not equal.
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+  //Assume that the objects match. Return false as soon as we find a single difference.
+  //Since the keys must be identical, then we must be able to access object2 data with the key from object1.
+  //And since the values must be identical, then the value at object2[key1] must be the same as the value at val1.
+  //So if they're not the same, the objects aren't equal.
+  for (let key1 of obj1Keys) {
+    const val1 = object1[key1];
+    const val2 = object2[key1];
+
+    //Are they both non-array objects, or arrays?
+    const bothObjects = !Array.isArray(val1) && !Array.isArray(val2) && (val1 instanceof Object) && (val2 instanceof Object);
+    const bothArrays = Array.isArray(val1) && Array.isArray(val2);
+
+    if (bothObjects && !eqObjects(val1, val2)) {
+      return false;
+    } else if (bothArrays && !eqArrays(val1, val2)) {
+      return false;
+    } else if (!bothObjects && !bothArrays && val1 !== val2) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const eqArrays = function(array1, array2) {
   //If one or the other isn't an array, return undefined.
   if (!(array1 instanceof Array) || !(array2 instanceof Array)) {
@@ -21,7 +52,21 @@ const eqArrays = function(array1, array2) {
 
   for (let i = 0; i < array1.length; i++) {
     // If at any point we find an unequal value, return false.
-    if (array1[i] !== array2[i]) {
+    // First: are the values we're comparing arrays? Or possibly objects?
+    const bothArrays = Array.isArray(array1[i]) && Array.isArray(array2[i]);
+    const bothObjects = !Array.isArray(array1[i]) &&
+      !Array.isArray(array2[i]) &&
+      array1[i] instanceof Object &&
+      array2[i] instanceof Object;
+    
+    if (bothArrays && !eqArrays(array1[i], array2[i])) {
+      //They're arrays, but not equal.
+      return false;
+    } else if (bothObjects && !eqObjects(array1[i], array2[i])) {
+      //They're objects, but not equal.
+      return false;
+    } else if (!bothObjects && !bothArrays && array1[i] !== array2[i]) {
+      //They're not both object or both arrays, so we can directly compare them.
       return false;
     }
   }
@@ -42,5 +87,6 @@ assertEqual(eqArrays("a", "b"), undefined);
 
 
 // Tests for nested arrays or arrays of objects. These should fail until the function is expanded.
-assertEqual(eqArrays([1, 2, [3]], [1, 2, [3]]), true);
-assertEqual(eqArrays([{beans : "magical"}] ,[{beans : "magical"}]), true);
+console.log("Recursive tests:");
+assertEqual(eqArrays([1, [[[[[[2, [3]]]], 4]]]], [1, [[[[[[2, [3]]]], 4]]]]), true);
+assertEqual(eqArrays([{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}] ,[{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}]), true);
