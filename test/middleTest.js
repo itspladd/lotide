65ece694b642d8a8030e92123414bc39d60dbfdc
Paul Ladd <vlendle@gmail.com>
2021-03-08 21:17:15 +0000

    Refactored to use mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,40 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([0,1,2,3,4,5]), [2,3]);
-assertArraysEqual(middle([0,3,4,5]), [3,4]);
-assertArraysEqual(middle(['0','1','2','3','4','5']), ['2','3']);
-assertArraysEqual(middle([0,1,3,4,5]), [3]);
-assertArraysEqual(middle([0]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle(), []);
+describe("#middle", () => {
+  it("should return the middle two integers of an integer array with even length", () => {
+    const input = [0,1,2,3,4,5];
+    const expected = [2,3];
+
+    assert.deepEqual(middle(input), expected);
+  });
+  it("should return the middle two strings of a string array with even length", () => {
+    const input = ['0','1','2','3','4','5'];
+    const expected = ['2','3'];
+    
+    assert.deepEqual(middle(input), expected);
+  });
+  it("should return the middle value of an array with odd length", () => {
+    const input = [0,1,3,4,5];
+    const expected = [3];
+
+    assert.deepEqual(middle(input), expected);
+  });
+  it("should return an empty array for an array with one value", () => {
+    const input = [0];
+    const expected = [];
+
+    assert.deepEqual(middle(input), expected);
+  });
+  it("should return an empty array for an empty array", () => {
+    const input = [];
+    const expected = [];
+
+    assert.deepEqual(middle(input), expected);
+  });
+  it("should return an empty array for no input", () => {
+    const expected = [];
+
+    assert.deepEqual(middle(), expected);
+  });
+});
