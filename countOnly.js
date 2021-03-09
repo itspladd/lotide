01dae3259d7c6227925d8287d45298f32c077e47
Paul Ladd <vlendle@gmail.com>
2021-03-09 02:42:00 +0000

    Refactored countOnly for mocha/chai testing

--- countOnly.js
+++ countOnly.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  const actualStr = typeof(actual) === "string" ? `"${actual}"` : actual;
-  const expectedStr = typeof(expected) === "string" ? `"${expected}"` : expected;
-  
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion passed: ${actualStr} === ${expectedStr}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion failed: ${actualStr} !== ${expectedStr}`);
-  }
-};
-
 const countOnly = function(arrayToCount, countableItems) {
+  if(!arrayToCount || !countableItems) {
+    return { };
+  }
+  
   const result = {};
   //Iterate through each item in the input array
   //If the key for that item has a truthy value, add or increment the appropriate key in the results
@@ -25,24 +18,5 @@ const countOnly = function(arrayToCount, countableItems) {
 
   return result;
 };
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna" : false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
 
 module.exports = countOnly;
---
+++ test/countOnlyTest.js
@@ -0,0 +1,59 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly');
+
+
+describe('#countOnly', () => {
+  it(`should return an object containing key/value pairs of array elements specified and the number of times that element was found`, () => {
+    const input = [1, 2, 3, 3, 2];
+    const toFind = { 1: true, 2: false, 3: true }
+    const expected = { "1": 1, "3": 2};
+  
+    assert.deepEqual(countOnly(input, toFind), expected);
+  });
+  it(`should return an empty object if no matches exist`, () => {
+    const input = ["1", "2", "3"];
+    const toFind = { 1: false, 4: true};
+    const expected = { };
+  
+    assert.deepEqual(countOnly(input, toFind), expected);
+  });
+  it(`should return an empty object if an empty array is given`, () => {
+    const input = [];
+    const toFind = {1: true};
+    const expected = {};
+
+    assert.deepEqual(countOnly(input, toFind), expected);
+  });
+  it(`should return an empty object if an empty toFind is given`, () => {
+    const input = [1, 2, 3];
+    const toFind = { };
+    const expected = { };
+  
+    assert.deepEqual(countOnly(input, toFind), expected);
+  });
+  it(`should return an empty object if no parameters are given`, () => {
+    const expected = { };
+  
+  
+    assert.deepEqual(countOnly(), expected);
+  });
+});
+
+/* const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna" : false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined); */
