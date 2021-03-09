e4efcca7cac325bf40dfb91136454b68b3f791a8
Paul Ladd <vlendle@gmail.com>
2021-03-09 05:01:53 +0000

    Refactored map to use mocha/chai testing

--- map.js
+++ map.js
@@ -1,28 +1,8 @@
-const eqArrays = function(array1, array2) {
-  // If the two arrays aren't of the same length, return false before we iterate.
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    // If at any point we find an unequal value, return false.
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  // If we make it this far, then all of the values are equal and we can return true.
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("✅✅✅Assertion passed: ", array1, " === ", array2);
-  } else {
-    console.log("🔴🔴🔴Assertion failed: ", array1, " !== ", array2);
-  }
-};
-
 const map = (array, callback) => {
+  if(!array || !callback) {
+    return [];
+  }
+  
   const results = [];
 
   for (let item of array) {
@@ -31,13 +11,5 @@ const map = (array, callback) => {
 
   return results;
 };
-/*
-const test1 = ["ground", "control", "to", "major", "tom"];
-const test2 = [1,2,3,4,5];
-const test3 = [{ name: "bob" }, { name: "martha" }, { name: "erin" }, { name: "taylor" }];
-
-assertArraysEqual(map(test1, x => x[1]), ["r", "o","o","a","o"]);
-assertArraysEqual(map(test2, x => x + 3), [4,5,6,7,8]);
-assertArraysEqual(map(test3, x => x.name), ["bob", "martha", "erin", "taylor", ]); */
 
 module.exports = map;
---
+++ test/mapTest.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert;
+const map = require('../map');
+
+
+describe('#map', () => {
+  it(`should return a new array as a result of calling the callback function on each element of the input array`, () => {
+    const input = ["ground", "control", "to", "major", "tom"];
+    const callback = x => x[1];
+    const expected = ["r", "o", "o", "a", "o"];
+  
+    assert.deepEqual(map(input, callback), expected);
+  });
+  it(`should function on arrays of objects`, () => {
+    const input = [{ name: "bob" }, { name: "martha" }, { name: "erin" }, { name: "taylor" }];
+    const callback = x => x.name;
+    const expected = ["bob", "martha", "erin", "taylor"];
+  
+  
+    assert.deepEqual(map(input, callback), expected);
+  });
+  it(`should return an empty array for empty input`, () => {
+    const input = [];
+    const callback = x => x > 500;
+    const expected = [];
+  
+  
+    assert.deepEqual(map(input, callback), expected);
+  });
+  it(`should not modify the original array`, () => {
+    const input = [1, 2, 3, 4, 5];
+    const callback = x => x * 2;
+    const expected = [1, 2, 3, 4, 5];
+    map(input, callback);
+  
+    assert.deepEqual(input, expected);
+  });
+  it(`should return an empty array for no input`, () => {
+    const expected = [];
+  
+    assert.deepEqual(map(), expected);
+  });
+});
+
+/*
+const test1 = ["ground", "control", "to", "major", "tom"];
+const test2 = [1,2,3,4,5];
+const test3 = [{ name: "bob" }, { name: "martha" }, { name: "erin" }, { name: "taylor" }];
+
+assertArraysEqual(map(test1, x => x[1]), ["r", "o","o","a","o"]);
+assertArraysEqual(map(test2, x => x + 3), [4,5,6,7,8]);
+assertArraysEqual(map(test3, x => x.name), ["bob", "martha", "erin", "taylor", ]); */
