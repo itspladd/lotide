172c023fca6639e2760a29c37d47537b445225ba
Paul Ladd <vlendle@gmail.com>
2021-03-13 20:51:30 +0000

    Updated eqArraysTest to use mocha/chai testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,19 +1,43 @@
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
-
-assertEqual(eqArrays([8, 6, 7, 5, 3, 0, 9], [8, 6, 7, 5, 3, 0, 9]), true);
-assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-//Tests for non-array values.
-assertEqual(eqArrays(1, [1]), undefined);
-assertEqual(eqArrays([1], 1), undefined);
-assertEqual(eqArrays("a", "b"), undefined);
+const assert = require('chai').assert;
+const eqArrays = require('../eqArrays');
 
 
-// Tests for nested arrays or arrays of objects. These should fail until the function is expanded.
-console.log("Recursive tests:");
-assertEqual(eqArrays([1, [[[[[[2, [3]]]], 4]]]], [1, [[[[[[2, [3]]]], 4]]]]), true);
-assertEqual(eqArrays([{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}] ,[{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}]), true);
+describe('#eqArrays', () => {
+  it(`should return true for two equal arrays of primitives`, () => {
+    const input = [8, 6, 7, '5', 3, 0, '9'];
+    const comparison = [8, 6, 7, '5', 3, 0, '9'];
+  
+    assert.isTrue(eqArrays(input, comparison));
+  });
+  it(`should return false for arrays of different lengths`, () => {
+    const input = [1, 2, 3];
+    const comparison = [1, 2, 3, 4];
+  
+    assert.isFalse(eqArrays(input, comparison));
+  });
+  it(`should return undefined for non-array inputs or missing inputs`, () => {
+    const input = [1, 2];
+    const comparison = '1, 2';
+    const comparison2 = { '1': '2'};
+  
+  
+    assert.isUndefined(eqArrays(input, comparison));
+    assert.isUndefined(eqArrays(input, comparison2));
+    assert.isUndefined(eqArrays(input));
+    assert.isUndefined(eqArrays());
+  });
+  it(`should return true for nested arrays`, () => {
+    const input = [1, [[[[[[2, [3]]]], 4]]]];
+    const comparison = [1, [[[[[[2, [3]]]], 4]]]];
+  
+  
+    assert.isTrue(eqArrays(input, comparison));
+  });
+  it(`should return true for arrays with nested objects and/or arrays of arbitrary depth`, () => {
+    const input = [{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}];
+    const comparison = [{beans : ["magical", "fruit", {the: "more", you: [[["eat"]]]}]}];
+  
+  
+    assert.isTrue(eqArrays(input, comparison));
+  });
+});
