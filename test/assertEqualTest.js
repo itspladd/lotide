4dbda00bc7bdf2cbcea746335217fe32514d2a0f
Paul Ladd <vlendle@gmail.com>
2021-03-13 20:50:59 +0000

    Added extra logging and argument-dependent output, so we don't have to see these logged every time we test

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,12 @@
 const assertEqual = require('../assertEqual');
+const verbose = process.argv[2];
 
-assertEqual("LighthouseLabs", "Bootcamp");
-assertEqual("pladd", "goober");
-assertEqual(1, 1);
-assertEqual("1", "1");
+if (verbose === '-v') {
+  console.log('Tests for assertEqual:')
+  console.log('The following assertions should fail:')
+  assertEqual("LighthouseLabs", "Bootcamp");
+  assertEqual("pladd", "goober");
+  console.log('The following assertions should pass:')
+  assertEqual(1, 1);
+  assertEqual("1", "1");
+}
